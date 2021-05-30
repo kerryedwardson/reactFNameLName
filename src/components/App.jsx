2027c1c175b1ad31ae8185179202423e05b203d6
Kerry Edwardson <57094218+kerryedwardson@users.noreply.github.com>
2021-05-30 13:24:51 +1000

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+
+function App() {
+  const [fName, setFName] = useState("");
+  const [lName, setLName] = useState("");
+  function updateFName(event) {
+    setFName(event.target.value);
+  }
+  function updateLName(event) {
+    setLName(event.target.value);
+  }
+  return (
+    <div className="container">
+      <h1>
+        Hello {fName} {lName}
+      </h1>
+      <form>
+        <input
+          name="fName"
+          onChange={updateFName}
+          placeholder="First Name"
+          value={fName}
+        />
+        <input
+          name="lName"
+          onChange={updateLName}
+          placeholder="Last Name"
+          value={lName}
+        />
+        <button>Submit</button>
+      </form>
+    </div>
+  );
+}
+
+export default App;
